2852d926a5292392a0b48a6c6e614d47bed42894
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-02-22 15:13:11 -0500

    setting up server.js and api routes

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,15 @@
 
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
-require('./routes/apiRoutes')(app);
-require('./routes/htmlRoutes')(app);
+require('./api/notes')(app);
 
 
 
