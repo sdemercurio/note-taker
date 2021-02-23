afee14d546d8c46467b87ef0b55bb9a89c90efb9
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-02-23 16:28:11 -0500

    links to open the note taker and back home work

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -6,11 +6,13 @@ const path = require('path');
 module.exports = (app) => {
 
 
-    // whent he user hits 
+    // when the user hits "Get Started" they are directed to notes.html
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
+
+// Directs user home
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 });
