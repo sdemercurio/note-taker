aca706af520ed641381420607463568cd695821a
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-02-23 15:52:37 -0500

    htmlRoutes should be set

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const noteStorage = require('../db/notes');
 
 
-// "get" retrieves notes
+// "get" reads notes from the db.json file
 router.get('/notes', (req, res) => {
     noteStorage
         .getNotes()
@@ -11,3 +11,20 @@ router.get('/notes', (req, res) => {
         })
         .catch((err) => res.status(500).json(err));
 });
+
+// // post recieves notes
+// router.post('/notes', (req, res) => {
+//     noteStorage
+//     .addNote(req.body)
+//     .then((noteStorage) => res.json(noteStorage))
+//     .catch((err) => res.status(500).json(err));
+// });
+
+// router.delete('/notes/:id', (req, res) => {
+//     noteStorage
+//         .removeNote(req.params.id)
+//         .then(() => res.json( { ok: true}))
+//         .catch((err) => res.status(500).json(err));
+// });
+
+module.exports = router;
