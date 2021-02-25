5836a00d8ca76635c36f7a301212fccd00a056ca
Sarah DeMercurio <sdemercuriomi@gmail.com>
2021-02-24 19:44:40 -0500

    fixing routes but can't seem to save

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 
 const express = require('express');
-const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes/htmlRoutes');
-const path = require('path');
 
 const app = express();
 
